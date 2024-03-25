f697a9734f4f7bb1a133df5db3fc3ff548a7f252
Lucio <80083192+luciobrito@users.noreply.github.com>
2024-03-25 18:48:41 -0300

    API: Novas Rotas

---
+++ api/app/Http/Controllers/PostController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    public function Create(Request $request){
+        $postData = $request->validate([
+            'title' => 'required',
+            'body'=> 'required',
+            'user_id']);
+        $postData['user_id'] = Auth::user()->id;
+        if($create = Post::create($postData)) return response()->json('Post criado com sucesso', 201);
+        else return response()->json('Não foi possível criar o Post', 500);
+    }
+    public function AuthUserPosts(){
+        return Post::where('user_id', Auth::user()->id)->simplePaginate(10);
+    }
+    public function Delete($id){
+        $post = Post::find($id);
+        if($post->user_id != Auth::user()->id) return response()->json('Não autorizado',401);
+        else {$post->delete(); return response()->json('Deletado com sucesso', 200);}
+        
+    }
+    /*Debug functions */
+    public function AllPosts(){
+        return Post::all();
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -8,7 +9,22 @@ use App\Http\Controllers\UserController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');*/
-Route::post('/user',[UserController::class,'Create']);
-Route::get('/user/{id}', [UserController::class,'getById']);
+
+//Rotas Anônimas
+Route::post('register',[UserController::class,'Create']);
 Route::post('login', [AuthController::class,'Login']);
-Route::middleware('auth:api')->get('/self', [UserController::class, 'self']);
+
+//Rotas autenticadas
+Route::middleware(['auth:api'])->group(function(){
+    Route::controller(UserController::class)->group(function(){
+        Route::get('user/self','self');
+    });
+    Route::controller(PostController::class)->group(function(){
+        Route::post('post', 'Create');
+        Route::get('user/posts', 'AuthUserPosts');
+        Route::delete('post/{id}', 'Delete');
+    });
+    
+});
+
+Route::get('users', [UserController::class, 'getAll']);
