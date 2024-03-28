811fa77c86b8ae6586da93bab2a78e55e761f83a
Lucio <80083192+luciobrito@users.noreply.github.com>
2024-03-28 19:34:05 -0300

    Front: Loading Spinner adicionado

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,5 +1,8 @@
 <template>
   <div v-if="error.status == 404">Esse usuário não existe :(</div>
+  <div v-else-if="error.status == 500">Serviço indisponível</div>
+  <div v-else-if="loading"><LoadingSpinner></LoadingSpinner></div>
+ 
   <div v-else>
     <h1>{{ user.name }}</h1>
     <i>@{{ user.username }}</i>
@@ -16,7 +19,11 @@
 import { useRoute } from "vue-router";
 import axios from "axios";
 import DateConvert from "../dateConverter";
+import LoadingSpinner from '../components/modules/LoadingSpinner.vue';
 export default {
+  components:{
+    LoadingSpinner
+  },  
   data() {
     return {
       user: {
@@ -26,14 +33,17 @@ export default {
       error: {
         status: 0,
       },
+      loading: false
     };
   },
   methods: {
     getUserData() {
+        this.loading = true;
       const route = useRoute();
-      var username = route.params.username;
-      axios(`/user/${username}`)
+      var username = route.params.username; 
+      setTimeout(()=>{ axios(`/user/${username}`)
         .then((response) => {
+        
           console.log(response.data);
           this.user = response.data;
           /*Converte a data de ISO para comum */
@@ -46,11 +56,12 @@ export default {
         .catch((error) => {
           console.log(error.response.status);
           this.error.status = error.response.status;
-        });
+        }).finally(()=>this.loading = false)}, 0);
     },
   },
   beforeMount() {
-    this.getUserData();
+    this.getUserData()
+    
   },
 };
 </script>
---
+++ src/components/modules/LoadingSpinner.vue
@@ -0,0 +1,38 @@
+<template>
+
+<div class="background"><p><span class="loader"></span></p></div>
+</template>
+
+<style lang="scss">
+.background{
+    background-color: rgba(0, 0, 0, 0.247);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    overflow: auto;
+}
+.loader {
+    width: 48px;
+    height: 48px;
+    border: 5px solid #ffffff00;
+    border-bottom-color: #0059ff;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+    }
+
+    @keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+    } </style>
